91d34a5927413ac4cf957486f0a1b619edcfffe7
Tom Hogans <tomhsx@gmail.com>
2014-01-23 02:40:48 -0500

    Redirect root URL to /admin until other views are implemented

--- inventory/urls.py
+++ inventory/urls.py
@@ -3,10 +3,8 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'inventory.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+urlpatterns = patterns(
+    '',
+    url(r'^$', 'inventory.views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
 )
